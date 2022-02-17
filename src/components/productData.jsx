b330650a3ff3ca0dabfca7d5656e4f32dc84143f
Marvin-kamwenji <marvinkamwenjih@gmail.com>
2022-02-17 15:32:19 +0300

    updates on product

--- src/components/productData.jsx
+++ src/components/productData.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-
-const products = [
+const data = {
+    products: [
       {
-        id: '1',
+        _id: '1',
         name: 'Explorer',
         imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.canr.msu.edu%2Fnews%2Fexploring_equine_breeds_the_arabian&psig=AOvVaw0LWLSnQ-yO9-jBD0mJqOHD&ust=1645175386029000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPi1mI2yhvYCFQAAAAAdAAAAABAJ',
         price: 'KSHS 120,000',     
@@ -11,7 +10,7 @@ const products = [
       },
 
       {
-        id: '2',
+        _id: '2',
         name: 'Blackie',
         imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fanimals.desktopnexus.com%2Fwallpaper%2F1395313%2F&psig=AOvVaw0LWLSnQ-yO9-jBD0mJqOHD&ust=1645175386029000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPi1mI2yhvYCFQAAAAAdAAAAABAc',
         price: 'KSHS 125,000',     
@@ -19,7 +18,7 @@ const products = [
         location: 'Githurai',
       },
       {
-        id: '3',
+        _id: '3',
         name: 'MO Poppies',
         imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thesprucepets.com%2Fmost-popular-horse-breeds-1886146&psig=AOvVaw0LWLSnQ-yO9-jBD0mJqOHD&ust=1645175386029000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPi1mI2yhvYCFQAAAAAdAAAAABA3',
         price: 'KSHS 220,000',     
@@ -27,7 +26,7 @@ const products = [
         location: 'Kajiado',
       },
       {
-        id: '4',
+        _id: '4',
         name: 'Isabel Pokorny',
         imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F410390584774616808%2F&psig=AOvVaw0LWLSnQ-yO9-jBD0mJqOHD&ust=1645175386029000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPi1mI2yhvYCFQAAAAAdAAAAABBA',
         price: 'KSHS 220,000',     
@@ -35,7 +34,7 @@ const products = [
         location: 'Muranga',
       },
       {
-        id: '5',
+        _id: '5',
         name: 'Aghale aphamesta',
         imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Flansdown1570%2Faqha-apha-stallions%2F&psig=AOvVaw0LWLSnQ-yO9-jBD0mJqOHD&ust=1645175386029000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPi1mI2yhvYCFQAAAAAdAAAAABBK',
         price: 'KSHS 178,000',     
@@ -43,7 +42,7 @@ const products = [
         location: 'Taita',
       },
       {
-        id: '6',
+        _id: '6',
         name: 'Gorgeous Overo',
         imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F536561743088923686%2F&psig=AOvVaw0LWLSnQ-yO9-jBD0mJqOHD&ust=1645175386029000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPi1mI2yhvYCFQAAAAAdAAAAABBU',
         price: 'KSHS 183,000',     
@@ -51,19 +50,5 @@ const products = [
         location: 'Isiolo',
       },
     ],
-
-
-
-function MarketPlace(){
-    return (
-       <div key={products.id} className="marketBlock">
-
-           <img src={products.imageUrl}/>
-           <h3>{products.name}</h3>
-           
-
-       </div>
-    );
-}
-
-export default MarketPlace;
+  };
+  export default data;
